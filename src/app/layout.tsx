78ca1162dabc76f82b574155e0583964eab4605f
Anup Kasula <anup.kasula@elscript.com>
2024-03-02 11:21:17 +0545

    change title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,8 +5,8 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Home | @nup Kasula",
-  description: "Home",
+  title: " @nup | Kasul@",
+  description: " @nup",
 };
 
 export default function RootLayout({
